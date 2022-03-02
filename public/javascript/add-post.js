bc64145dcc0f6efad65e6f992f4eb2adabf21a3b
Ward <wardv@tirf.ca>
2022-03-02 18:47:08 -0500

    added upvoting points

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,45 +1,18 @@
 
-// const skateArray = [];
-
-// function figureSkatesHandler(event) {
-//   event.preventDefault();
-//   document.getElementById("mySkates").innerHTML = "Figure skates";
-//   skateArray.push("Figure skates");
-//   console.log(skateArray)
-// } 
-
-// function hockeySkatesHandler() {
-//   console.log("hockey");
-//   document.getElementById("mySkates").innerHTML = "Hockey skates";
-//   skateArray.push("Hockey skates");
-// } 
-
-// function raceSkatesHandler() {
-//   console.log("race");
-//   document.getElementById("mySkates").innerHTML = "Race skates";
-//   skateArray.push("Race skates");
-// } 
-
-// function touringSkatesHandler() {
-//   console.log("tour");
-//   document.getElementById("mySkates").innerHTML = "Touring skates";
-//   skateArray.push("Touring skates");
-// }
 
 async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
   const post_text = document.querySelector('textarea[name="post-text"]').value;
-  // let my_skates = skateArray.values();
-  // console.log(my_skates);
+  
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
     body: JSON.stringify({
       title,
       post_text,
-      // my_skates,
+    
     }),
     headers: {
       "Content-Type": "application/json",
