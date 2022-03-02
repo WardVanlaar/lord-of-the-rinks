d228ce8f752900ff31671f602ef01ae59b16968a
Ward <wardv@tirf.ca>
2022-03-02 16:27:56 -0500

    working on vote count

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,38 +1,45 @@
-// function figureSkatesHandler() {
-//   console.log("figure");
-//   document.getElementById('mySkates').innerHTML = "Figure skates"
-// }
+
+// const skateArray = [];
+
+// function figureSkatesHandler(event) {
+//   event.preventDefault();
+//   document.getElementById("mySkates").innerHTML = "Figure skates";
+//   skateArray.push("Figure skates");
+//   console.log(skateArray)
+// } 
 
 // function hockeySkatesHandler() {
 //   console.log("hockey");
-//   document.getElementById('mySkates').innerHTML = "Hockey skates"
-// }
+//   document.getElementById("mySkates").innerHTML = "Hockey skates";
+//   skateArray.push("Hockey skates");
+// } 
 
 // function raceSkatesHandler() {
 //   console.log("race");
-//   document.getElementById('mySkates').innerHTML = "Race skates"
-// }
+//   document.getElementById("mySkates").innerHTML = "Race skates";
+//   skateArray.push("Race skates");
+// } 
 
 // function touringSkatesHandler() {
 //   console.log("tour");
-//   document.getElementById('mySkates').innerHTML = "Touring skates"
+//   document.getElementById("mySkates").innerHTML = "Touring skates";
+//   skateArray.push("Touring skates");
 // }
-
 
 async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
   const post_text = document.querySelector('textarea[name="post-text"]').value;
-  let my_skates = document.querySelectorAll('input[name="skate"]:checked');
-  console.log(my_skates)
+  // let my_skates = skateArray.values();
+  // console.log(my_skates);
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
     body: JSON.stringify({
       title,
       post_text,
-      my_skates
+      // my_skates,
     }),
     headers: {
       "Content-Type": "application/json",
@@ -47,5 +54,5 @@ async function newFormHandler(event) {
 }
 
 document
-  .querySelector(".new-post-form")
-  .addEventListener("submit", newFormHandler);
+  .querySelector("#createPost")
+  .addEventListener("click", newFormHandler);
