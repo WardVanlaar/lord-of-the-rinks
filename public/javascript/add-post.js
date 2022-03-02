0c41f3b34c9b2ceb7ecfec35b159232761a0c839
Ward <wardv@tirf.ca>
2022-03-02 10:21:35 -0500

    working on selecting skates

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -3,24 +3,42 @@ async function newFormHandler(event) {
 
   const title = document.querySelector('input[name="post-title"]').value;
   const post_text = document.querySelector('textarea[name="post-text"]').value;
-  // const post_text = document.querySelector('input[name="post-text"]').value;
+
+  function figureSkatesHandler() {
+    document.getElementById("mySkates").innerHTML = "Figure skates";
+  }
+
+  function hockeySkatesHandler() {
+    document.getElementById("mySkates").innerHTML = "Hockey skates";
+  }
+
+  function raceSkatesHandler() {
+    document.getElementById("mySkates").innerHTML = "Race skates";
+  }
+
+  function touringSkatesHandler() {
+    document.getElementById("mySkates").innerHTML = "Touring skates";
+  }
 
   const response = await fetch(`/api/posts`, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify({
       title,
-      post_text
+      post_text,
+      mySkates,
     }),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
 
   if (response.ok) {
-    document.location.replace('/dashboard');
+    document.location.replace("/dashboard");
   } else {
     alert(response.statusText);
   }
 }
 
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
