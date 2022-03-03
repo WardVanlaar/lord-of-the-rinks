fbc333651a195a2f587a239304668912d1bc231d
Ward <wardv@tirf.ca>
2022-03-02 20:46:28 -0500

    fixed vote points

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,6 +12,7 @@ router.get('/', (req, res) => {
         'post_text',
         'user_id',
         'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,18 +1,46 @@
 
+// const skateArray = [];
+
+// function figureSkatesHandler(event) {
+//   event.preventDefault();
+//   document.getElementById("mySkates").innerHTML = "Figure skates";
+//   skateArray.push("Figure skates");
+//   console.log(skateArray)
+// } 
+
+// function hockeySkatesHandler() {
+//   console.log("hockey");
+//   document.getElementById("mySkates").innerHTML = "Hockey skates";
+//   skateArray.push("Hockey skates");
+// } 
+
+// function raceSkatesHandler() {
+//   console.log("race");
+//   document.getElementById("mySkates").innerHTML = "Race skates";
+//   skateArray.push("Race skates");
+// } 
+
+// function touringSkatesHandler() {
+//   console.log("tour");
+//   document.getElementById("mySkates").innerHTML = "Touring skates";
+//   skateArray.push("Touring skates");
+// }
+// var my_skates = ""
 
 async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
   const post_text = document.querySelector('textarea[name="post-text"]').value;
-  
+  // let my_skates = skateArray.values();
+  // console.log(my_skates);
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
     body: JSON.stringify({
       title,
       post_text,
-    
+      // my_skates,
     }),
     headers: {
       "Content-Type": "application/json",
